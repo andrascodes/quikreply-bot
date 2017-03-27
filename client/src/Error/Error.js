2106e1035e657c989fef38443d0fabcc3ff72c86
Andrew Szucs <andrew0szucs@gmail.com>
2017-03-27 23:11:55 +0200

    feat(comps): add Error and Loading component

---
+++ client/src/Error/Error.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Error = props => (
+  <div className="container-fluid">
+    <h2>Error. Try again later!</h2>
+  </div>
+)
---
+++ client/src/Loading/Loading.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Loading = props => (
+  <div className="container-fluid">
+    <h2>Loading...</h2>
+  </div>
+)
