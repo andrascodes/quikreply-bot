46f4fa737244a1ffb55cf811891e2bf98a3446d7
Andrew Szucs <andrew0szucs@gmail.com>
2017-03-27 23:13:45 +0200

    feat(index): add new dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Navbar, Sidebar } from './Navigation'
 import { Main } from './Main'
 import { Conversations } from './Conversations'
+import { Messages } from './Messages'
+import { Dashboard } from './Dashboard'
 
 import './App.css';
 
@@ -17,9 +19,9 @@ class App extends Component {
           />
           <Sidebar />
           <Main>
-            <Route exact path="/" />
+            <Route exact path="/" component={Dashboard}/>
             <Route exact path="/conversations" component={Conversations} />
-            <Route path="/conversations/:id" />
+            <Route path="/conversations/:id" component={Messages}/>
           </Main>
         </div>
       </Router>
